56873f6ff13ff500cbb4087ead9af704795224e6
sergeysergeevru <me@sergeysergeev.ru>
2018-07-16 12:32:11 +0300

    added tests for yml and json local configuration

--- config_structures_test.go
+++ config_structures_test.go
@@ -45,3 +45,49 @@ type sub2LevelConfig struct {
 type levelConfig struct {
 	Root subLevelConfig `yaml:"level_1_1"`
 }
+
+var realLevelConfig = levelConfig{
+	Root: subLevelConfig{
+		SubRootOne: sub2LevelConfig{
+			SubSubRoot: configTypes{
+				Int64:   9223372036854775807,
+				Int32:   2147483647,
+				Float64: 65777.03,
+				Float32: 54.05,
+				String:  "lol",
+				Bool:    true,
+			},
+		},
+		SubRootTwo: configTypes{
+			Int64:   -9223372036854775808,
+			Int32:   -2147483648,
+			Float64: -65777.03,
+			Float32: -54.05,
+			String:  "",
+			Bool:    false,
+		},
+	},
+}
+
+var realConfigLocal = levelConfig{
+	Root: subLevelConfig{
+		SubRootOne: sub2LevelConfig{
+			SubSubRoot: configTypes{
+				Int64:   986655,
+				Int32:   2147,
+				Float64: 5434.55,
+				Float32: 657.03,
+				String:  "local config",
+				Bool:    false ,
+			},
+		},
+		SubRootTwo: configTypes{
+			Int64:   -922337203,
+			Int32:   -214,
+			Float64: -5324.15,
+			Float32: -65777.03,
+			String:  "",
+			Bool:    true,
+		},
+	},
+}
